9e91684556fc16a8c95014254a65bec0f2ae3fa6
user <d_k81@mail.ru>
2025-06-15 21:42:14 +0500

    add assert

--- src/assert_check.py
+++ src/assert_check.py
@@ -1,4 +1,3 @@
-
 def add_numbers(a, b):
     return a+b
 
@@ -8,17 +7,17 @@ def is_even(a):
 
 
 def find_max(my_list):
-    return max(my_list)
+    if len(my_list) > 0:
+        return max(my_list)
+    return 0
 
 
 if __name__ == '__main__':
     assert add_numbers(2, 2) == 4
 
-    assert is_even(2) == True
+    assert is_even(0) == True
 
     assert find_max([1, 2, 3, 4, 5]) == 5
+    assert find_max([]) == 0
 
-    # from src.utils import reverse_text
-    #
-    # if __name__ == '__main__':
-    #     reverse_text('кораблик')
+
--- src/main.py
+++ src/main.py
@@ -1,18 +1,4 @@
-def add_numbers(a, b):
-    return a+b
+from src.utils import reverse_text
 
-
-def is_even(a):
-    return a % 2 == 0
-
-
-def find_max(my_list):
-    return max(my_list)
-
-
-if __name__ == '__main__':
-    assert add_numbers(2, 2) == 4
-
-    assert is_even(2) == True
-
-    assert find_max([1, 2, 3, 4, 5]) == 5
+    if __name__ == '__main__':
+        reverse_text('кораблик')
