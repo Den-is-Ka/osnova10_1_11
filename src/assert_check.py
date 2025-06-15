2257865145ea250cc4d592f6f840749c7ac292b0
user <d_k81@mail.ru>
2025-06-15 21:19:52 +0500

    add assert

--- src/assert_check.py
+++ src/assert_check.py
@@ -1,3 +1,4 @@
+
 def add_numbers(a, b):
     return a+b
 
@@ -16,3 +17,8 @@ if __name__ == '__main__':
     assert is_even(2) == True
 
     assert find_max([1, 2, 3, 4, 5]) == 5
+
+    # from src.utils import reverse_text
+    #
+    # if __name__ == '__main__':
+    #     reverse_text('кораблик')
